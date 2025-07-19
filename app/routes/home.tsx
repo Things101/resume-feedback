9ff1160df2ab902dc2799b948e8227cf807719e4
Irwin <carpool7200@gmail.com>
2025-07-19 13:28:29 -0400

    Completed basic header template and resume cards

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import { resumes } from "~/constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Feedback" },
+    { name: "description", content: "Improvements for your resume" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications and Resume Ratings</h1>
+          <h2>Review your resume for AI-powered feeback.</h2>
+        </div>
+      </section>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
